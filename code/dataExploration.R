1882744274e0079b4e9a9de2e1db0baae8b46676
mlevy <malevy@ucdavis.edu>
2015-06-26 13:59:53 -0700

    with prediction script

--- code/dataExploration.R
+++ code/dataExploration.R
@@ -1,4 +1,5 @@
 setwd(file.path('~', 'GitHub', 'FireAtTheFringe'))  # Set working directory
+source("code/dataCleaning.R")
 d = read.csv('data/cleanData.csv')  # Read the data in
 library(ggplot2)
 library(reshape2)
@@ -58,8 +59,8 @@ t[complete.cases(t), ] %>%
 # from some analyses. They only make up 5% of the dataset:
 table(t$own)
 
-# d[d$a5 == 2, ] %>% # Run this line instead of the next to plot only homeowners
-d %>%
+d[d$a5 == 2, ] %>% # Run this line instead of the next to plot only homeowners
+# d %>%
 ggplot(aes(x = f5b, y = sumDSBehavior)) +
   geom_point(position = "jitter") +
   geom_smooth(method = "lm") +
@@ -107,95 +108,3 @@ ggplot(d, aes(x = town, y = sumDSBehavior)) +
 
 # Note some e1-Qs  (ds beliefs) are reverse coded
 
-# Create a new dataframe having removed a few variables that can't be used for prediction
-# What variables have a lot of missingness?
-sort(sapply(d, function(x) sum(is.na(x))))
-
-# F2xx questions need to be removed (1's are the response, 2's are too close, I think)
-d2 = d[, !grepl("^f2", names(d))]
-
-# a5xx are involvement of home modification/construction.
-# These could be informative, but missing lots of entries, so ofr now removing
-d2 = d2[, !grepl("^a5.", names(d2))]
-
-# Some misc weird variables
-d2 = subset(d2, select = -(c(X, address, state, id, b1, i2b, i2c)))
-str(d2, list.len = 999)  # Not sure what the next-to-last ~10 columns are. 
-# Look like derived variables. Perhaps they should be deleted???
-
-# Some sub-question in income and ethinicity that were rarely answered
-d2 = subset(d2, select = -c(j5c, j6c, j5a, j6a))
-
-# Where did you live before moving here has too many levels
-d2 = d2[, !grepl("b3.", names(d2))]
-
-length(unique(substr(d2$zip, 1, 5)))  # 11 unique zips, 10 cities, so these are likely too colinear
-d2 = subset(d2, select = -zip)
-
-# Merge a8 (lot size) answer in sqft and acres and remove the other
-d2$a8acres = ifelse(is.na(d2$a8acres) & !is.na(d2$a8sqfeet),
-                    d2$a8sqfeet / 43560,
-                    d2$a8acres)
-d2 = subset(d2, select = -a8sqfeet)
-
-# Still have missing data on the majority of rows.
-sum(complete.cases(d2)) / nrow(d2)
-
-# So, let's impute. Will want to do something more principled eventually, for now jsut mean impute
-library(DMwR)
-dImp = knnImputation(d2, k = 3, scale = TRUE)
-
-# Let's try some prediction
-# Using random forests
-library(randomForest)
-set.seed(123)
-# Reserve 1/3 of the data for testing
-trainingRows = sample(1:nrow(dImp), nrow(dImp) * .67) 
-rf1 = randomForest(sumDSBehavior ~ ., data = dImp, subset = trainingRows,
-                   importance = TRUE)
-rf1Yhat = predict(rf1, newdata = dImp[-trainingRows, ])
-mean((rf1Yhat - dImp[-trainingRows, "sumDSBehavior"])^2)  # Test MSE
-varImpPlot(rf1, n.var = 10)  # What are the key var's in the dataset for predicting DS Behav?
-
-# Using boosted trees
-library(gbm)
-set.seed(90210)
-D = 2
-lam = .1
-boost1 = gbm(sumDSBehavior ~ ., data = dImp[trainingRows, ],
-          distribution = "gaussian", n.trees = 5e3,
-          interaction.depth = D, shrinkage = lam)
-head(summary(boost1))
-plot(boost1, i = "city")
-plot(boost1, i = "f3e1")  # f3b1 U-shaped too. Weird.
-boost1Yhat = predict(boost1, newdata = dImp[-trainingRows, ], n.trees = 5e3)
-mean((boost1Yhat - dImp[-trainingRows, "sumDSBehavior"])^2)  # Worse than RF
-
-# Let's try tuning the tuning parameters
-boostedModels = list()
-boostedModels = 
-  lapply(1:4, function(D) {
-    lapply(10^(-1:-4), function(lam) {
-      structure(
-        gbm(sumDSBehavior ~ ., data = dImp[trainingRows, ],
-          distribution = "gaussian", n.trees = 5e3,
-          interaction.depth = D, shrinkage = lam),
-        "param" = paste0("D=", D, " lambda=", lam))
-    })
-  })
-boostedModels = unlist(boostedModels, recursive = FALSE)
-
-# Calculate MSE for each model on test data
-boostedMSEs = 
-  sapply(boostedModels, function(x) {
-    mod = attr(x, "param")
-    yhat = predict(x, newdata = dImp[-trainingRows, ], n.trees = 5e3)
-    MSE = mean((yhat - dImp[-trainingRows, "sumDSBehavior"])^2)
-    list(mod, MSE)
-  }) 
-
-bestModIndex = which.min(boostedMSEs[2, ])
-boostedMSEs[, bestModIndex]  # Beats RF by .35
-bestBoosted = boostedModels[[bestModIndex]] # d=3, lam=.001
-plot(bestBoosted)
-head(summary(bestBoosted))
---
+++ code/prediction.R
@@ -0,0 +1,105 @@
+setwd(file.path('~', 'GitHub', 'FireAtTheFringe'))  # Set working directory
+source("code/dataCleaning.R")
+d = read.csv('data/cleanData.csv')  # Read the data in
+
+dsBehavior = names(d)[grepl("f2.1", names(d))]
+d$sumDSBehavior = apply(d[dsBehavior], 1, 
+                        function(x) sum(x, na.rm = TRUE))
+
+str(d, list.len = 999)
+# Not sure what the next-to-last ~10 columns are. 
+# Look like derived variables. Perhaps they should be deleted?
+# They are constructed scales and should be deleted
+
+# What variables have a lot of missingness?
+sort(sapply(d, function(x) sum(is.na(x))))
+
+# Create a new dataframe removed a few variables that can't be used for prediction
+
+# F2xx questions need to be removed (1's are the dependent variable, 
+# 2's (future behavior) are too close, I think)
+d2 = d[, !grepl("^f2", names(d))]
+
+# a5xx are involvement of home modification/construction.
+# These could be informative, but missing lots of entries, so for now removing
+d2 = d2[, !grepl("^a5.", names(d2))]
+
+# Some misc weird variables
+d2 = subset(d2, select = -(c(X, address, state, id, b1, i2b, i2c)))
+
+# Some sub-question in income and ethinicity that were rarely answered
+d2 = subset(d2, select = -c(j5c, j6c, j5a, j6a))
+
+# Where did you live before moving here has too many levels
+d2 = d2[, !grepl("b3.", names(d2))]
+
+length(unique(substr(d2$zip, 1, 5)))  # 11 unique zips, 10 cities, so these are likely too colinear
+d2 = subset(d2, select = -zip)
+
+# Merge a8 (lot size) answer in sqft and acres and remove the other
+d2$a8acres = ifelse(is.na(d2$a8acres) & !is.na(d2$a8sqfeet),
+                    d2$a8sqfeet / 43560,
+                    d2$a8acres)
+d2 = subset(d2, select = -a8sqfeet)
+
+sum(complete.cases(d2)) / nrow(d2)
+# Still have missing data on the majority of rows.
+
+# So, let's impute. Will want to do something more principled eventually, for now just use KNN
+library(DMwR)
+dImp = knnImputation(d2, k = 3, scale = TRUE)
+
+# Let's try some prediction!
+# Using random forests
+library(randomForest)
+set.seed(123)
+# Reserve 1/3 of the data for testing
+trainingRows = sample(1:nrow(dImp), nrow(dImp) * .67) 
+rf1 = randomForest(sumDSBehavior ~ ., data = dImp, subset = trainingRows,
+                   importance = TRUE)
+rf1Yhat = predict(rf1, newdata = dImp[-trainingRows, ])
+mean((rf1Yhat - dImp[-trainingRows, "sumDSBehavior"])^2)  # Test MSE
+varImpPlot(rf1, n.var = 10)  # What are the key var's in the dataset for predicting DS Behav?
+
+# Using boosted trees
+library(gbm)
+set.seed(90210)
+D = 2
+lam = .1
+boost1 = gbm(sumDSBehavior ~ ., data = dImp[trainingRows, ],
+             distribution = "gaussian", n.trees = 5e3,
+             interaction.depth = D, shrinkage = lam)
+head(summary(boost1))  # Note the plot probably won't display all the variable names
+plot(boost1, i = "city")
+plot(boost1, i = "f3e1")  # f3b1 U-shaped too. Weird.
+boost1Yhat = predict(boost1, newdata = dImp[-trainingRows, ], n.trees = 5e3)
+mean((boost1Yhat - dImp[-trainingRows, "sumDSBehavior"])^2)  # Worse than RF
+
+# Let's try tuning the tuning parameters
+boostedModels = list()
+boostedModels = 
+  lapply(1:4, function(D) {
+    lapply(10^(-1:-4), function(lam) {
+      structure(
+        gbm(sumDSBehavior ~ ., data = dImp[trainingRows, ],
+            distribution = "gaussian", n.trees = 5e3,
+            interaction.depth = D, shrinkage = lam),
+        "param" = paste0("D=", D, " lambda=", lam))
+    })
+  })
+boostedModels = unlist(boostedModels, recursive = FALSE)
+
+# Calculate MSE for each model on test data
+boostedMSEs = 
+  sapply(boostedModels, function(x) {
+    mod = attr(x, "param")
+    yhat = predict(x, newdata = dImp[-trainingRows, ], n.trees = 5e3)
+    MSE = mean((yhat - dImp[-trainingRows, "sumDSBehavior"])^2)
+    list(mod, MSE)
+  }) 
+
+bestModIndex = which.min(boostedMSEs[2, ])
+boostedMSEs[, bestModIndex]  # Beats RF by .35
+bestBoosted = boostedModels[[bestModIndex]] # d=3, lam=.001
+plot(bestBoosted)
+head(summary(bestBoosted))
