4979b199a1c4ed5a14b0d0994531737198894d2e
mlevy <malevy@ucdavis.edu>
2015-07-29 15:31:54 -0400

    Including distance to burn and CNF as predictors

--- code/prediction.R
+++ code/prediction.R
@@ -2,6 +2,9 @@ setwd(file.path('~', 'GitHub', 'FireAtTheFringe'))  # Set working directory
 source("code/dataCleaning.R")
 d = read.csv('data/cleanData.csv')  # Read the data in
 source("code/addNeighborsDSBehavior.R")
+dists = read.csv("data/withDist.csv")
+dists = subset(dists, select = c("ID", "near_cnf", "near_03f"))
+d = merge(d, dists, by.x = "id", by.y = "ID")
 
 #compute current DS behavior
 # dsBehavior = names(d)[grepl("f2.1", names(d))]
