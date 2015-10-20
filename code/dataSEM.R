1b048dcf7d4ea7e288fef33639e5ac86d689b83f
katiemlyon <katie.lyon@colostate.edu>
2015-10-20 15:09:11 -0600

    reordered behavior variables

--- code/dataSEM.R
+++ code/dataSEM.R
@@ -5,7 +5,10 @@ dists = read.csv("data/withDist.csv")
 dists = subset(dists, select = c("ID", "near_cnf", "near_03f"))
 d = merge(d, dists, by.x = "id", by.y = "ID")
 
-sem  <- subset(d, select = c(id, c1a:c1r, f1a:f1g, f2a1:f2g2, f5a:f5c, neighbors_f2a1:near_03f))
+sem  <- subset(d, select = c(id, c1a:c1r, f1a:f1g, f2a1, f2b1, f2c1, f2d1, f2e1, f2f1, f2g1, f2a2, f2b2, f2c2, f2d2, f2e2, f2f2, f2g2, f5a:f5c, d1h:d1j, g2a:g2d, h1a:h1c, neighbors_f2a1:near_03f))
+
+# Write data for sem analysis to csv file
+write.csv(sem, 'data/semdata.csv', row.names = FALSE)
 
 # Write data for sem analysis in mplus to csv file
 write.table(sem, 'mplus/mplusdata.csv', row.names = FALSE, col.names = FALSE, na = "99", sep=",")
