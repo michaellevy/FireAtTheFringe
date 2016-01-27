9a564b8bfedaf3337cc494bbce6fc6988a89496e
katiemlyon <katie.lyon@colostate.edu>
2016-01-27 11:22:06 -0500

    edit code for sem data

--- code/dataSEM.R
+++ code/dataSEM.R
@@ -6,26 +6,11 @@ dists = subset(dists, select = c("ID", "near_cnf", "near_03f"))
 d = merge(d, dists, by.x = "id", by.y = "ID")
 
 # Subset data for sem
-sem  <- subset(d, select = c(id, c1a:c1r, f1a:f1g, f2a1, f2b1, f2c1, f2d1, f2e1, f2f1, f2g1, f2a2, f2b2, f2c2, f2d2, f2e2, f2f2, f2g2, f5a:f5c, d1h:d1j, g2a:g2d, h1a:h1c, neighbors_f2a1:near_03f))
+sem  <- subset(d, select = c(id, e1a, e1c, e1f, e1g, f5a:f5c, f1a:f1g, f2a1, f2b1, f2c1, f2d1, f2e1, f2f1, f2g1, f2a2, f2b2, f2c2, f2d2, f2e2, f2f2, f2g2, h1a:h1c, near_03f))
 
 
 # Write data for sem analysis to csv file
 write.csv(sem, 'data/semdata.csv', row.names = FALSE)
 
-# Write data for sem analysis in mplus to csv file
+# Write data for sem analysis in mplus to csv file (no column names)
 write.table(sem, 'mplus/mplusdata.csv', row.names = FALSE, col.names = FALSE, na = "99", sep=",")
-
-
-
-# Subset data for covariance matrix
-sem2 <- subset(d, select = c(c1a:c1r, f1a:f1g, f2a1, f2b1, f2c1, f2d1, f2e1, f2f1, f2g1, f2a2, f2b2, f2c2, f2d2, f2e2, f2f2, f2g2, f5a:f5c, d1h:d1j, g2a:g2d, h1a:h1c, neighbors_f2a1:near_03f))
-
-# Assign NA to missing values that were coded 99 or 999:
-sem2[sem2 == 99 | sem2 == 999] <- NA  
-
-# Get covariance matrix
-cov <- cov(sem2, use = "pairwise.complete.obs")
-!cov <- cov(sem2, use = "complete.obs") # missing deleted casewise
-
-# Write covariance matrix to csv file
-write.csv(cov, 'data/covariance.csv')
